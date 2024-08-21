892e723b6e56b32955cde5d42491a11eaa19ad14
Diego Mirhan <mirhan.diego@gmail.com>
2024-08-21 14:38:55 -0300

    frontend - fixed aria attribute . 21/08

--- frontend/src/components/menu/headerMenu.js
+++ frontend/src/components/menu/headerMenu.js
@@ -28,9 +28,7 @@ export function HeaderMenu() {
       {/* Drawer Mobile */}
       <Drawer>
         <DrawerTrigger asChild>
-          <div className="md:hidden cursor-pointer">
-            <Image width={32} height={32} alt="menu icon svg" src="/menu.svg"></Image>
-          </div>
+            <Image width={32} height={32} alt="menu icon svg" src="/menu.svg" className="md:hidden cursor-pointer"></Image>
         </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
@@ -44,9 +42,7 @@ export function HeaderMenu() {
       {/* Sheet Desktop */}
       <Sheet>
         <SheetTrigger asChild>
-          <div className="hidden md:inline cursor-pointer">
-            <Image width={32} height={32} alt="menu icon svg" src="/menu.svg"></Image>
-          </div>
+          <Image width={32} height={32} alt="menu icon svg" src="/menu.svg" className="hidden md:inline cursor-pointer"></Image>
         </SheetTrigger>
         <SheetContent className="flex flex-col overflow-auto">
           <SheetHeader>

ad41008460e665940488db6f9dc71f1b1f26179c
Diego Mirhan <mirhan.diego@gmail.com>
2024-09-07 10:11:20 -0300

    removed ad cards

--- src/components/ads/ads.js
+++ src/components/ads/ads.js
@@ -1,8 +1,8 @@
 export function VerticalAds() {
     return (
         <>
-            <div className="hidden lg:flex bg-gray-300 h-96 w-40 items-center justify-center rounded-lg font-semibold">
-                GOOGLE ADS
+            <div>
+                
             </div>
         </>
     )
@@ -13,8 +13,8 @@ export function HorizontalAds() {
 
   return (
     <>
-      <div className="flex bg-gray-300 h-32 w-4/5 items-center justify-center rounded-lg font-semibold m-auto">
-        GOOGLE ADS
+      <div >
+        
       </div>
     </>
   );

d275138251421bb2cbd978e03af8dd8cb3f87c06
Diego Mirhan <mirhan.diego@gmail.com>
2024-10-05 12:59:31 -0300

    ads disabled

--- src/components/google/adsense.js
+++ src/components/google/adsense.js
@@ -1,6 +1,7 @@
 export function AdsBanner ({ adWidth = 300, adHeight = 250 }) {
   return (
-    <iframe
+    <>
+    {/*<iframe
       src="//www.topcreativeformat.com/watchnew?key=d5a2afd458e99a60cd1a38281b199144"
       width={adWidth}
       height={adHeight} 
@@ -8,6 +9,7 @@ export function AdsBanner ({ adWidth = 300, adHeight = 250 }) {
       scrolling="no"
       className='mx-auto flex my-5 flex-row justify-center items-center'
       title="Anúncio do Adsterra"
-    ></iframe>
+    ></iframe> */}
+    </>
   );
 };

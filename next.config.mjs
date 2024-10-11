07195e0963f329ccc6ff50c7c1ee417de6e059b1
Diego Mirhan <mirhan.diego@gmail.com>
2024-10-11 16:30:06 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontEndNav: true,
   reloadOnOnline: true,
-  disable: true,
+  disable: false,
   workboxOptions: {
     disableDevLogs: true,
     runtimeCaching: [
